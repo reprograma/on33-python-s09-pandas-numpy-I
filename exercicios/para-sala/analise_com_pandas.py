719362de6b2c9cfc305623374239416c014c08e9
Manuelly Suzik <manuellysuzik@gmail.com>
2024-08-08 01:45:46 -0300

    mudar nome atividade sala

---
+++ exercicios/para-sala/analise_com_pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(
+    "C:/Projetos/Reprograma/on33-python-s09-pandas-numpy-I/exercicios/para-sala/despesasPorOrgao.csv",
+    delimiter=";",
+)
