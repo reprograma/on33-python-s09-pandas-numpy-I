d8f51c21b80ac491ebb2131311de2156c5560659
Elysa Mirela <elysacarv@gmail.com>
2024-08-14 00:39:19 -0300

    atividade de casa semana 11

--- exercicios/para-sala/ETL_pandas.py
+++ exercicios/para-sala/ETL_pandas.py
@@ -2,25 +2,21 @@ import pandas as pd
 
 # ['TransactionID', 'Date', 'MobileModel', 'Brand', 'Price', 'UnitsSold','TotalRevenue', 'CustomerAge', 'CustomerGender', 'Location','PaymentMethod']
 
-df = pd.read_csv(
-    "C:/Projetos/Reprograma/on33-python-s09-pandas-numpy-I/material/mobile_sales.csv"
-)
+df = pd.read_csv(r"C:/Users/bruno/Desktop/REPROG/on33-python-s09-pandas-numpy-I/material/mobile_sales.csv")
 
 print(df.head())
 print(df.columns)
 df_valores_nulos = df.isnull()
 print(df_valores_nulos.sum())
-print(df.duplicated().sum())
+print(df.duplicated())
 print(df.dtypes)
 
 df["Date"] = pd.to_datetime(df["Date"], format="mixed")
-
 print(df.dtypes)
 
 df["Total Sales Value"] = df["Price"] * df["UnitsSold"]
 print(df.columns)
 profit_per_product = 0.30
-
 df["Profit Margin"] = (df["Price"] * profit_per_product) * df["UnitsSold"]
 
 filtered_df = df[(df["Total Sales Value"] > 100_000) & (df["Profit Margin"] > 20_000)]
@@ -28,3 +24,5 @@ filtered_df = df[(df["Total Sales Value"] > 100_000) & (df["Profit Margin"] > 20
 print(filtered_df.head())
 
 filtered_df.to_csv("./exercicios/para-sala/filtered_list.csv", index=False)
+
+
