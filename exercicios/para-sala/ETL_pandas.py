8ed5e093f8c561be12f783b8ebf705443fbdc2ec
Guilhermina Pereira da Silva <mina@velicastelo.art>
2024-08-15 21:15:03 +0100

    atv_casa_v1

--- exercicios/para-sala/ETL_pandas.py
+++ exercicios/para-sala/ETL_pandas.py
@@ -1,30 +1,62 @@
 import pandas as pd
+#Index(['TransactionID', 'Date', 'MobileModel', 'Brand', 'Price', 'UnitsSold',
+#       'TotalRevenue', 'CustomerAge', 'CustomerGender', 'Location',
+#       'PaymentMethod']
 
-# ['TransactionID', 'Date', 'MobileModel', 'Brand', 'Price', 'UnitsSold','TotalRevenue', 'CustomerAge', 'CustomerGender', 'Location','PaymentMethod']
+#Extração de dados(extract):
+df = pd.read_csv("material/mobile_sales.csv") 
+#df = dataframe, é um dado tabular, contém colunas, linhas e índices. 
+#É necessário que qualquer arquivo torne-se um
+#dataframe para que possamos usar neles funções.
 
-df = pd.read_csv(
-    "C:/Projetos/Reprograma/on33-python-s09-pandas-numpy-I/material/mobile_sales.csv"
-)
+print(df.head(n = 5))
+# #imprime as cinco primeiras linhas de uma tabela cvs. 
+# # Pode se colocar imputs nele por ex.: df.head(n=5) ou df.head(5) para exibir as
+# #cinco primeiras linhas. Se deixar em branco ele exibe, por padrão, cinco linhas.
 
-print(df.head())
-print(df.columns)
-df_valores_nulos = df.isnull()
-print(df_valores_nulos.sum())
-print(df.duplicated().sum())
-print(df.dtypes)
+print(df.columns) #o mesmo raciocínio da função a cima, mas ele exibe uma lista
+# #com nome de todas as colunas existentes no arquivo.
 
-df["Date"] = pd.to_datetime(df["Date"], format="mixed")
+# #Tratamento de dados(transform):
+print(df.isnull()) #função que retorna uma expressão boleana 
+# #'True' se a célula tem algum valor nulo, ou 'False' para as
+# #que foram preenchidas.
 
-print(df.dtypes)
+df_valores_nulos = df.isnull() #Guardamos todos os valores nulos numa variável
+# print(df_valores_nulos.sum()) #outra forma de ver valores nulos em números, somados. 
 
-df["Total Sales Value"] = df["Price"] * df["UnitsSold"]
-print(df.columns)
-profit_per_product = 0.30
+print(df.duplicated()) #Retorna boleanos 'True' se encontram linhas duplicadas 
+# #e 'False' caso contrário.
 
-df["Profit Margin"] = (df["Price"] * profit_per_product) * df["UnitsSold"]
+print(df.dtypes) #função que me ajuda a saber (neste caso, exibe) os tipos de dados dentro de um dataframe. 
+# #Retorna o tipo de dado contido em cada coluna. OBS: object=string.
 
-filtered_df = df[(df["Total Sales Value"] > 100_000) & (df["Profit Margin"] > 20_000)]
+# #formato != tipo
+# #formato é como ele está escrito
 
-print(filtered_df.head())
+df["Date"] = pd.to_datetime(df["Date"], format = "mixed") #Função do pandas, para converter tipos de dados.
+# print(df.dtypes) # Mostrar uma coluna inteira
+# print(df["Date"])
 
-filtered_df.to_csv("./exercicios/para-sala/filtered_list.csv", index=False)
+df["Total Sales Value"] = df["Price"] * df["UnitsSold"] #Operação matemática que multiplica os valores de uma
+#dada coluna pela outra ao mesmo tempo que cria outra.
+# print(df["Total Sales Value"])
+
+#Exercício:
+#Crie uma coluna 'Profit Margin', assumindo que o custo de fabricação é 70% do preço ('Price'), 
+#a margem de lucro pode ser calculada como (Price * 0.30) * UnitsSold.
+df["Desconto"] = df["Price"] * 0.30
+# print(df["Desconto"])
+
+df["Profit Margin"] = df["Desconto"] * df["UnitsSold"] #Operação matemática que multiplica os valores de uma
+#dada coluna pela outra ao mesmo tempo que cria outra.
+# print(df["Profit Margin"])
+# print(df.columns)
+
+#Filtragem
+filtered_df = df [(df["Total Sales Value"] > 100_000) & (df["Profit Margin"] > 20_000)]
+print(filtered_df)
+
+#carregamento:
+filtered_df.to_csv("/Users/minavelicastelo/Library/Mobile Documents/com~apple~CloudDocs/Reprograma/on33-python-s09-pandas-numpy-I/exercicios/para-sala/filterd_list.csv", index=False)# a operação boleana 'index=False' 
+#significa que o pandas não manda o index para o arquivo novo criado.
